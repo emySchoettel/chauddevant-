3fa7f91ac583b97fb91bcc14f5ffae775fc6ab31
emySchoettel <e.schoettel@ludus-academie.com>
2021-09-12 17:18:54 +0200

    Revert "fix ui credits"
    This reverts commit 450e0fa80bc0ae5b242738b0fa12d8970a2bdc7a.

--- Assets/Scripts/EcranCreditManager.cs
+++ Assets/Scripts/EcranCreditManager.cs
@@ -12,7 +12,6 @@ public class EcranCreditManager : MonoBehaviour
         credits[0].text = Translation.Get("credits.musique.titre");
         credits[1].text = Translation.Get("credits.musique.bgm.ecrantitre");
         credits[2].text = Translation.Get("credits.musique.bgm.jeu");
-        credits[5].text = Translation.Get("credits.assets");
 
         credits[3].text = Translation.Get("credits.equipe.titre");
         credits[4].text = Translation.Get("credits.equipe.equipe");
